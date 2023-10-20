00f4d6e7c121f8aabe2b5ebe6994e310d39ab6b6
sara rezaei <rezaiesara1994@gmail.com>
2023-10-20 12:48:05 +0330

    json added & fetch

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-let drinks = '{ "drink" : [' +
-'{"artikel":"Getränk 1", "preis":"1,19€"},'+
-'{"artikel":"Getränk 2", "preis":"2,15€"},'+
-'{"artikel":"Getränk 3", "preis":"3,49€"},'+
-'{"artikel":"Getränk 4", "preis":"1,86€"},'+
-'{"artikel":"Getränk 5", "preis":"1,39€"},'+
-'{"artikel":"Getränk 6", "preis":"1,06€"},'+
-'{"artikel":"Getränk 7", "preis":"0,20€"},'+
-'{"artikel":"Getränk 8", "preis":"2,20€"},'+
-'{"artikel":"Getränk 9", "preis":"1,40€"}]}';
+
+let http = new XMLHttpRequest();
 
 
+http.open('get', 'data.json', true);
 
-const drinksObj = JSON.parse(drinks);
-const drinksEl = document.getElementById('drink1');
 
-function handleClick1() {
-  drinksEl.innerHTML = drinksObj.drink[0].artikel + " " + drinksObj.drink[0].preis;
+http.send();
+
+
+http.onload = function(){
+  
+  if(this.readyState == 4 && this.status == 200){
+
+    let data = JSON.parse(this.responseText);
+
+    let output = "";
+
+
+    for(let item of data){
+      output += `
+        <div class="data">
+          <p>${item.artikel}</p>
+          <p>${item.preis}</p>
+        </div>
+      `;
+    } 
+
+    document.querySelector(".data").innerHTML = output;
+  }
 }
 
-drinksEl.addEventListener('click', handleClick1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const drinksObj = JSON.parse(drinks);
+// const drinksEl = document.getElementById('drink1');
+
+// function handleClick1() {
+//   drinksEl.innerHTML = drinksObj.drink[0].artikel + " " + drinksObj.drink[0].preis;
+// }
+
+// drinksEl.addEventListener('click', handleClick1);
 
 
 // fetch data from json
