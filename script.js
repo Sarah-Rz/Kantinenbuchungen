96c93faee78e0e3f4a458ae78f5f9d2d9b55775d
sara rezaei <rezaiesara1994@gmail.com>
2023-10-20 21:42:52 +0330

    left Buttons worked

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Getränk 1
 function handleClick1() {
 // fetch data
 let http = new XMLHttpRequest();
@@ -20,7 +21,7 @@ http.onload = function(){
     let output = "";
 
 
-      output = `
+      output += `
         <div class="data">
           <table>
             <tr>
@@ -32,7 +33,199 @@ http.onload = function(){
       `;
     
 
-    document.querySelector(".data").innerHTML = output;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 2
+function handleClick2() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[1].artikel}</td>
+              <td>${data[1].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 3
+function handleClick3() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[2].artikel}</td>
+              <td>${data[2].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 4
+function handleClick4() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[3].artikel}</td>
+              <td>${data[3].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 5
+function handleClick5() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[4].artikel}</td>
+              <td>${data[4].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 6
+function handleClick6() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[5].artikel}</td>
+              <td>${data[5].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 7
+function handleClick7() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[6].artikel}</td>
+              <td>${data[6].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 8
+function handleClick8() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[7].artikel}</td>
+              <td>${data[7].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
+  }
+}
+}
+
+// Getränk 9
+function handleClick9() {
+let http = new XMLHttpRequest();
+http.open('get', 'data.json', true);
+http.send();
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    let output = "";
+      output += `
+        <div class="data">
+          <table>
+            <tr>
+              <td>${data[8].artikel}</td>
+              <td>${data[8].preis}</td>
+            </tr>
+          </table>
+        </div>
+      `;
+    document.querySelector(".data").innerHTML += output;
   }
 }
 }
