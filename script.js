a37dec6f37b53482762f4e0d1d5a0a17d998aa70
sara rezaei <rezaiesara1994@gmail.com>
2023-10-23 13:13:21 +0330

    popup created

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let handleEl = document.getElementById("data_save");
 
 const btnChoose = document.querySelectorAll(".food");
 
-let count = 0;
+let countBtOne = 0;
+let countBtTwo = 0;
 let countNum = document.getElementById('countNum');
 
 let price = document.getElementById('price');
@@ -11,11 +12,22 @@ let price = document.getElementById('price');
 
 // Getränk 1
 function handleClick1() {
- 
-  count = count + 1;
-  countNum.innerHTML = count;  
 
-  document.querySelector("#countNum").innerHTML = count;
+  countBtOne += 1;
+  countNum.innerHTML = countBtOne;  
+
+
+
+
+
+  // let outputCount = "";
+
+  // outputCount += `
+  //   <div class="dataCount">
+  //     ${countBtOne}
+  //   </div>
+  // `
+  // document.querySelector(".dataCount").innerHTML = countBtOne;
 
 
 // fetch data
@@ -29,23 +41,33 @@ http.send();
 
 
 http.onload = function(){
+// if the button clicked for the first Time (count = 1), write the name of the food on Screen and for another Time, 
+// Button is clicked Just increase the count of selected food
+  if(countBtOne === 1) {
+    if(this.readyState == 4 && this.status == 200){
+
+      let data = JSON.parse(this.responseText);
   
-  if(this.readyState == 4 && this.status == 200){
-
-    let data = JSON.parse(this.responseText);
-
+      let output = "";
+  
+      output += `
+          <div class="data">
+            ${data[0].artikel}
+          </div>
+        `
+      document.querySelector(".data").innerHTML += output;
+     
+  }
+  } else {
     let output = "";
+      document.querySelector(".data").innerHTML += output;
 
-    output += `
-        <div class="data">
-          ${data[0].artikel}
-        </div>
-      `
-    document.querySelector(".data").innerHTML += output;
+  } 
+  
+}
+}
 
-
-    
-  //   let sum = 0;    
+//   let sum = 0;    
   //   let price = data[0].preis;
   //   sum = sum + parseFloat(price);
   //   countsNum = count.push(sum)
@@ -53,31 +75,49 @@ http.onload = function(){
   
   // console.log(typeof countsNum, countsNum);
 
-}
-  
-}
-}
-
 
 
 
 // Getränk 2
 function handleClick2() {
+  
+  
+  countBtTwo += 1;
+  countNum.innerHTML = countBtTwo;  
+
+
+
+
+  // let outputCount = "";
+
+  // outputCount += `
+  //   <div class="dataCount">
+  //     ${countBtTwo}
+  //   </div>
+  // `
+  // document.querySelector(".dataCount").innerHTML = countBtTwo;
+
 let http = new XMLHttpRequest();
 http.open('get', 'data.json', true);
 http.send();
 http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
+  if(countBtTwo === 1) {
+    if(this.readyState == 4 && this.status == 200){
+      let data = JSON.parse(this.responseText);
+      let output = "";
       output += `
-        <div class="data">
-          ${data[1].artikel}
-          ${data[1].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
+          <div class="data">
+            ${data[1].artikel}
+          </div>
+        `
+      document.querySelector(".data").innerHTML += output;
+     
   }
+  } else {
+    let output = "";
+      document.querySelector(".data").innerHTML += output;
+
+  } 
 }
 };
 
@@ -224,9 +264,18 @@ http.onload = function(){
 // delete all datas
 del = () => {
   handleEl.innerText = " "
-  count = 0;
 }
 
+// popup message
+let popup = document.getElementById("popup")
+
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
 
 
 
