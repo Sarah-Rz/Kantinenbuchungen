3a91000894d2038e4b3a9528130d527d3f7d8c53
sara rezaei <rezaiesara1994@gmail.com>
2023-10-23 23:09:22 +0330

    anzahl worked

--- script.js
+++ script.js
@@ -1,269 +1,126 @@
-// data from right-up the App
-let handleEl = document.getElementById("data_save");
+// SELECT FOODS BUTTON (left side)
+const foodsEl = document.querySelector(".foods");
+// ORDERED FOODS (right side)
+const orderedFoodsEl = document.querySelector(".ordered-foods");
 
-const btnChoose = document.querySelectorAll(".food");
-
-let countBtOne = 0;
-let countBtTwo = 0;
-let countNum = document.getElementById('countNum');
-
-let price = document.getElementById('price');
-
-
-// Getränk 1
-function handleClick1() {
-
-  countBtOne += 1;
-  countNum.innerHTML = countBtOne;  
-
-
-
-
-
-  // let outputCount = "";
-
-  // outputCount += `
-  //   <div class="dataCount">
-  //     ${countBtOne}
-  //   </div>
-  // `
-  // document.querySelector(".dataCount").innerHTML = countBtOne;
-
-
-// fetch data
-let http = new XMLHttpRequest();
-
-
-http.open('get', 'data.json', true);
-
-
-http.send();
-
-
-http.onload = function(){
-// if the button clicked for the first Time (count = 1), write the name of the food on Screen and for another Time, 
-// Button is clicked Just increase the count of selected food
-  if(countBtOne === 1) {
-    if(this.readyState == 4 && this.status == 200){
-
-      let data = JSON.parse(this.responseText);
-  
-      let output = "";
-  
-      output += `
-          <div class="data">
-            ${data[0].artikel}
-          </div>
-        `
-      document.querySelector(".data").innerHTML += output;
-     
+// RENDER FOODS
+function renderProducts() {
+  // fetch JSON
+  let http = new XMLHttpRequest();
+  http.open('get', 'data.json', true);
+  http.send();
+  http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    
+    // render foods
+    data.forEach( (food) => {
+      foodsEl.innerHTML += `
+      <button type="button" class="btn food" id="d-1" onclick= "addToOrder(${food.id})">${food.name}</button>
+      `;
+    })
   }
-  } else {
-    let output = "";
-      document.querySelector(".data").innerHTML += output;
+}
+}
+renderProducts();
 
+//ORDER ARRAY
+let order = [];
+
+// ADD TO ORDER
+function addToOrder(id) {
+  // fetch JSON
+  let http = new XMLHttpRequest();
+  http.open('get', 'data.json', true);
+  http.send();
+  http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let data = JSON.parse(this.responseText);
+    
+    // check if selected food already exist in order
+    // if some food in the order has the same id as this id in function addToOrder(id)
+    if (order.some((food) => food.id === id)) {
+      changeNumberOfFoods("plus", id);
+    } else {
+      // add selected food to order with special id
+      const food = data.find((food) => food.id === id);
+      order.push({
+        //change the structure of food data, push an object to array: save all the old property of foods using (...food), and add a new property for "Anzahl" area
+        ...food,
+        numberOfFoods: 1
+      }); 
+    }
+  }
+}
+
+updateOrder();
+}
+
+// UPDATE ORDER
+function updateOrder() {
+  renderOrderedFoods();
+  // renderSumPrice()
+}
+
+// RENDER ORDERED FOODS
+function renderOrderedFoods() {
+   // fetch JSON
+   let http = new XMLHttpRequest();
+   http.open('get', 'data.json', true);
+   http.send();
+   http.onload = function(){
+   if(this.readyState == 4 && this.status == 200){
+     let data = JSON.parse(this.responseText);
+
+     orderedFoodsEl.innerHTML = "";  //clear orders (order area), to prevent duplicate items
+     order.forEach((food) => {
+      orderedFoodsEl.innerHTML += `
+        <div class="row">
+          <div class="col-lg-6 data-1">
+              <div>
+                  <p class="data" id="data-1">${food.name}</p>
+              </div>
+          </div>
+          <div class="col-lg-6 grid-count-price data-2">
+              <div>
+                  <p class="dataCount" id="countNum">${food.numberOfFoods}</p>
+              </div>
+              <div>
+                  <p class="data" id="price">${food.price}€</p>
+              </div>
+          </div> 
+        </div>
+      `
+    })
+   }    
   } 
   
 }
+
+//INCREASE THE NUMBER OF SELECTED FOODS
+function changeNumberOfFoods(action, id) {
+  order = order.map((food) => {
+    let numberOfFoods = food.numberOfFoods;
+
+    if (food.id === id) {
+    //Maximale Anzahl der verfügbaren Getränke sind 9
+      if (action === "plus" && numberOfFoods < food.amount) {
+        numberOfFoods++;
+      }
+    }
+    return {
+      ...food,
+      numberOfFoods
+    };
+  });
+
+  updateOrder()
 }
 
-//   let sum = 0;    
-  //   let price = data[0].preis;
-  //   sum = sum + parseFloat(price);
-  //   countsNum = count.push(sum)
-  //   ;
-  
-  // console.log(typeof countsNum, countsNum);
-
-
-
-
-// Getränk 2
-function handleClick2() {
-  
-  
-  countBtTwo += 1;
-  countNum.innerHTML = countBtTwo;  
-
-
-
-
-  // let outputCount = "";
-
-  // outputCount += `
-  //   <div class="dataCount">
-  //     ${countBtTwo}
-  //   </div>
-  // `
-  // document.querySelector(".dataCount").innerHTML = countBtTwo;
-
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(countBtTwo === 1) {
-    if(this.readyState == 4 && this.status == 200){
-      let data = JSON.parse(this.responseText);
-      let output = "";
-      output += `
-          <div class="data">
-            ${data[1].artikel}
-          </div>
-        `
-      document.querySelector(".data").innerHTML += output;
-     
-  }
-  } else {
-    let output = "";
-      document.querySelector(".data").innerHTML += output;
-
-  } 
-}
-};
-
-// Getränk 3
-function handleClick3() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-       <div class="data">
-          ${data[2].artikel}
-          ${data[2].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 4
-function handleClick4() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[3].artikel}
-          ${data[3].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 5
-function handleClick5() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[4].artikel}
-          ${data[4].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 6
-function handleClick6() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[5].artikel}
-          ${data[5].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 7
-function handleClick7() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[6].artikel}
-          ${data[6].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 8
-function handleClick8() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[7].artikel}
-          ${data[7].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
-
-// Getränk 9
-function handleClick9() {
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let data = JSON.parse(this.responseText);
-    let output = "";
-      output += `
-        <div class="data">
-          ${data[8].artikel}
-          ${data[8].preis}
-        </div>
-      `;
-    document.querySelector(".data").innerHTML += output;
-  }
-}
-}
 
 // delete all datas
 del = () => {
-  handleEl.innerText = " "
+  orderedFoodsEl.innerText = " "
 }
 
 // popup message
